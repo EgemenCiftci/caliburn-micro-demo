b8d4d7936ced3e7066c0c2e2d57a566509d013c4
Egemen Çiftci <egemenciftci@gmail.com>
2022-10-17 23:59:51 +0200

    Add project files.

---
+++ CaliburnMicroDemo/ViewModels/FirstPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+
+namespace CaliburnMicroDemo.ViewModels;
+
+public class FirstPageViewModel : PropertyChangedBase
+{
+    private string _message = "Yolo";
+
+    public string Message
+    {
+        get => _message;
+        set
+        {
+            _message = value;
+            NotifyOfPropertyChange(() => Message);
+        }
+    }
+
+    private int _pressCount = 0;
+
+    public void ChangeMessage()
+    {
+        _pressCount++;
+        Message = $"Presses = {_pressCount}";
+    }
+}
---
+++ CaliburnMicroDemo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+
+namespace CaliburnMicroDemo.ViewModels;
+
+public class ShellViewModel : Conductor<object>
+{
+    public ShellViewModel()
+    {
+        ShowFirstPage();
+    }
+
+    public void ShowFirstPage()
+    {
+        _ = ActivateItemAsync(new FirstPageViewModel());
+    }
+}
